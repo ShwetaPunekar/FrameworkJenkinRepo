42abfd1cd1d762a24f52226866c0892044d6cbdd
shwet <shwet@ShwetaPunekar.mshome.net>
2024-06-29 12:27:53 +0530

    first push

---
+++ sampleprogs/src/test/java/JenkinSample/ContactTest.java
@@ -0,0 +1,18 @@
+package JenkinSample;
+
+import org.testng.annotations.Test;
+
+public class ContactTest 
+{
+	@Test
+	public void createContactTest()
+	{
+		System.out.println("Execute createContactTest");
+	}
+	
+	@Test
+	public void modifyContactTest()
+	{
+		System.out.println("Execute modifyCreateTese");
+	}
+}
